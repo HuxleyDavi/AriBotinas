b230a92b518ec96b81108279b6fdb014056f8ca1
HuxleyDavi <92643240+HuxleyDavi@users.noreply.github.com>
2022-07-05 14:16:34 -0300

    Add files via upload

---
+++ js/slides.js
@@ -0,0 +1,25 @@
+$(".carrosel").slick({
+	dots: true,
+	centerMode: true,
+	slidesToShow: 4,
+	arrows: false,
+	responsive:[
+
+		{
+			breakpoint:768,
+			settings:{
+				centerMode:true,
+				slidesToShow:2
+			}
+		},
+
+		{
+			breakpoint:580,
+			settings:{
+				arrows:false,
+				centerMode:true,
+				slidesToShow:1
+			}
+		},
+	]
+});
