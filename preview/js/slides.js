8da307ac7175c2bc3aae94f5f15e465f61f39774
Hx <92643240+HuxleyDavi@users.noreply.github.com>
2022-07-18 14:27:08 -0300

    new photos and organization

---
+++ preview/js/slides.js
@@ -0,0 +1,33 @@
+/*<!--//--------------------------By HS Corp-------------------------------//-->
+<!--//---------------------------@hs.corp-------------------------------//-->
+<!--//-----------------------hscorpp@gmail.com-------------------------------//-->*/
+
+$(".carrosel").slick({
+	dots: true,
+	centerMode: true,
+	slidesToShow: 4,
+	arrows: false,
+	responsive:[
+
+		{
+			breakpoint:768,
+			settings:{
+				centerMode:true,
+				slidesToShow:2
+			}
+		},
+
+		{
+			breakpoint:580,
+			settings:{
+				arrows:false,
+				centerMode:true,
+				slidesToShow:1
+			}
+		},
+	]
+});
+
+/*<!--//--------------------------By HS Corp-------------------------------//-->
+<!--//---------------------------@hs.corp-------------------------------//-->
+<!--//-----------------------hscorpp@gmail.com-------------------------------//-->*/
